06acb8bd71b0451df341bda239029f887ad76a5b
DogeBud <47661290+DogeBud@users.noreply.github.com>
2019-05-06 09:26:57 +0200

    Create Suspect.java

---
+++ src/model/Suspect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Jurad0
+ */
+public class Suspect {
+    
+    public String acts;
+    public Photo photos;
+    public String firstname;
+    public String surname;
+    public String dni;
+    public String domicile;
+    public String address;
+    public String registration;
+    
+    
+    
+    
+    
+}
