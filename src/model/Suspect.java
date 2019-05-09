59d050db0a40abdd2fb7c73b569648b1c9867a18
jinkirito <47738894+jinkirito@users.noreply.github.com>
2019-05-09 09:11:35 +0200

    subiendo clase completa Suspect

--- src/model/Suspect.java
+++ src/model/Suspect.java
@@ -7,18 +7,77 @@ package model;
 
 /**
  *
- * @author Jurad0
+ * @author Antonio Jose Adamuz Sereno
  */
 public class Suspect {
     
-    public String acts;
-    public String photos;
-    public String firstname;
-    public String surname;
-    public String dni;
-    public String domicile;
-    public String address;
-    public String registration;
+    private int CodeSuspect;
+    private String name;
+    private String lastname1;
+    private String lastname2;
+    private int CCompanions;
+    private String Record;
+    private String Facts;
+    
+    public Suspect(int CodeSuspect, String name, String lastname1,
+            String lastname2, int CCompanions, String Record, String Facts){
+            super();
+            this.CodeSuspect=CodeSuspect;
+            this.name=name;
+            this.lastname1=lastname1;
+            this.lastname2=lastname2;
+            this.CCompanions=CCompanions;
+            this.Record=Record;
+            this.Facts=Facts;
+    }
+    
+    public int getCodeSuspect(){
+        return CodeSuspect;
+    }
+    
+    public void setCodeSuspect(int CodeSuspect){
+        this.CodeSuspect=CodeSuspect;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    
+     public String getLastname1(){
+        return lastname1;
+    }
+    
+    public void setLastname1(String lastname1){
+        this.lastname1=lastname1;
+    }
+    
+    public String getLastname2(){
+        return lastname2;
+    }
+    
+    public void setLastname2(String lastname2){
+        this.lastname2=lastname2;
+    }
+    
+    public int getCCompanions(){
+        return CCompanions;
+    }
+    
+    public void setCCompanions(int CCompanions){
+        this.CCompanions=CCompanions;
+    }
+    
+    public String getRecord(){
+        return Record;
+    }
+    
+    public void setRecord(String Record){
+        this.Record=Record;
+    }
     
     
     
