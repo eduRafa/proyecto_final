56e3e6f57fd433a14633825b513025940d96fb3f
DogeBud <47661290+DogeBud@users.noreply.github.com>
2019-05-13 10:02:00 +0200

    Update Car_Registration.java

--- src/model/Car_Registration.java
+++ src/model/Car_Registration.java
@@ -5,8 +5,6 @@ package model;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 /**
  *
  * @author Jurad0
@@ -19,18 +17,6 @@ public Car_Registration(String registration) {
     this.registration=registration;
     
 }
-
-<<<<<<< HEAD
-=======
-public boolean validate (String registration){
-    boolean validate=false;    
-    Pattern p = Pattern.compile("^[0-9]{4}[a-zA-Z]{3}$");
-    Matcher m = p.matcher(this.registration);
-    if (m.find()){}
-    
-        return true;
-}
->>>>>>> f526a2cd25aea849a915f210a056e774f727b507
 
 public String getRegistration(){
         return registration;
